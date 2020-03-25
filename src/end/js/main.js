44879250559aabb5cbec266091517960d867d11f
David Elisha <kingdaviinchi@gmail.com>
2020-03-25 10:37:49 +0100

    updated UI

--- src/end/js/main.js
+++ src/end/js/main.js
@@ -1,11 +1,11 @@
 const menu = document.getElementById("menu");
 const menuContent = document.getElementById("menu-content");
-const nav = document.getElementById("nav");
+const navbar = document.getElementById("navbar");
 
 menu.addEventListener("click", e => {
   menu.classList.toggle("open");
   menuContent.classList.toggle("open");
   setTimeout(() => {
-    nav.classList.toggle("open");
+    navbar.classList.toggle("open");
   }, 300);
 });
